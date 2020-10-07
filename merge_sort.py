d50d5260a392bd6ecabb571ed9b8254947cbfa44
prackode <72190853+prackode@users.noreply.github.com>
2020-10-08 00:42:36 +0530

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,11 +1,16 @@
+# Python program for implementation of MergeSort
+# Merge Sort is a Divide and Conquer algorithm which divides the input array in two halves, calls itself for the two halves and then merges the two sorted halves...
+
+# Code:
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def merge_sort(values): 
   
     if len(values)>1: 
         m = len(values)//2
         left = values[:m] 
         right = values[m:] 
-        left = merge_sort(left) 
-        right = merge_sort(right) 
+        left = merge_sort(left)   # Recursively sorting the first half
+        right = merge_sort(right)  # Recursively sorting the second half
   
         values =[] 
   
@@ -23,3 +28,11 @@ def merge_sort(values):
             values.append(i) 
                   
     return values
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+'''Merge sort analysis:
+ Time Complexity: Sorting arrays on different machines. Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+                  T(n) = 2T(n/2) + \Theta(n)
+The above recurrence can be solved either using Recurrence Tree method or Master method. It falls in case II of Master Method and solution of the recurrence is 'nLogn'.
+Time complexity of Merge Sort is 'nLogn' in all 3 cases (worst, average and best) as merge sort always divides the array into two halves and take linear time to merge two halves
+'''
